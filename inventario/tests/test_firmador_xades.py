a78cf57829767563f5f1ddde66ac244f2347bbda
Camilo Andrés Calderón <cacalderon@pucesd.edu.ec>
2025-10-15 16:51:57 -0500

    Ajusta algoritmos declarados en firmas XAdES

--- inventario/tests/test_firmador_xades.py
+++ inventario/tests/test_firmador_xades.py
@@ -1,6 +1,9 @@
+import base64
+import hashlib
+import io
 import os
-import sys
 import random
+import sys
 import types
 
 os.environ.setdefault('DATABASE_URL', 'sqlite:///test_db.sqlite3')
@@ -8,14 +11,19 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sistema.settings')
 
 import django
 import pytest
+from copy import deepcopy
+
 from django.apps import apps
 from django.core.files.base import ContentFile
 from django.db import connection, models as dj_models
 from django.conf import settings
+from lxml import etree
 
 django.setup()
 
 from inventario.models import Empresa, Opciones
+from cryptography.hazmat.primitives import hashes
+
 from inventario.sri import firmador_xades
 
 
@@ -154,3 +162,125 @@ def test_firmar_xml_usa_certificado_por_empresa(monkeypatch, tmp_path):
     opciones_b.delete()
     empresa_a.delete()
     empresa_b.delete()
+
+
+def test_firmado_utiliza_hash_de_signature_method(monkeypatch, tmp_path):
+    """La firma debe utilizar el hash declarado en SignatureMethod."""
+
+    firmas_root = tmp_path / 'firmas'
+    firmas_root.mkdir()
+    monkeypatch.setattr(settings, 'FIRMAS_ROOT', str(firmas_root), raising=False)
+
+    def simple_save(self, *args, **kwargs):
+        dj_models.Model.save(self, *args, **kwargs)
+
+    monkeypatch.setattr(Opciones, 'save', simple_save, raising=False)
+
+    empresa = Empresa.objects.create(razon_social='Empresa SHA1', ruc='1790012345001')
+    opciones = Opciones.objects.create(
+        empresa=empresa,
+        identificacion=empresa.ruc,
+        password_firma='clave',
+    )
+    opciones.firma_electronica.save('empresa_sha1.p12', ContentFile(b'PKCS12-SHA1'), save=True)
+
+    class DummyPrivateKey:
+        def __init__(self):
+            self.algoritmos = []
+
+        def sign(self, data, _padding, algorithm):
+            self.algoritmos.append(algorithm)
+            return b'signature'
+
+    dummy_private_key = DummyPrivateKey()
+
+    class DummyCertificate:
+        def public_bytes(self, _encoding):
+            return b'certificate-der'
+
+    def fake_load_key_and_certificates(p12_bytes, password, backend=None):
+        return dummy_private_key, DummyCertificate(), []
+
+    monkeypatch.setattr(
+        firmador_xades.pkcs12,
+        'load_key_and_certificates',
+        fake_load_key_and_certificates,
+    )
+
+    class DummyBES:
+        def enveloped(self, xml_data, certificate, cert_der, signproc, tspurl=None, tspcred=None):
+            root = etree.fromstring(xml_data)
+            ns = 'http://www.w3.org/2000/09/xmldsig#'
+            signature = etree.Element(f'{{{ns}}}Signature')
+            signed_info = etree.SubElement(signature, f'{{{ns}}}SignedInfo')
+            etree.SubElement(
+                signed_info,
+                f'{{{ns}}}CanonicalizationMethod',
+                Algorithm='http://www.w3.org/2001/10/xml-exc-c14n#',
+            )
+            etree.SubElement(
+                signed_info,
+                f'{{{ns}}}SignatureMethod',
+                Algorithm='http://www.w3.org/2000/09/xmldsig#rsa-sha1',
+            )
+            reference = etree.SubElement(signed_info, f'{{{ns}}}Reference', URI='')
+            transforms = etree.SubElement(reference, f'{{{ns}}}Transforms')
+            etree.SubElement(
+                transforms,
+                f'{{{ns}}}Transform',
+                Algorithm='http://www.w3.org/2000/09/xmldsig#enveloped-signature',
+            )
+            etree.SubElement(
+                transforms,
+                f'{{{ns}}}Transform',
+                Algorithm='http://www.w3.org/2001/10/xml-exc-c14n#',
+            )
+            etree.SubElement(
+                reference,
+                f'{{{ns}}}DigestMethod',
+                Algorithm='http://www.w3.org/2000/09/xmldsig#sha1',
+            )
+            etree.SubElement(reference, f'{{{ns}}}DigestValue').text = ''
+            etree.SubElement(signature, f'{{{ns}}}SignatureValue').text = ''
+            root.append(signature)
+            return etree.ElementTree(root)
+
+    fake_endesive = types.ModuleType('endesive')
+    fake_xades = types.ModuleType('endesive.xades')
+    fake_xades.BES = DummyBES
+    fake_endesive.xades = fake_xades
+    monkeypatch.setitem(sys.modules, 'endesive', fake_endesive)
+    monkeypatch.setitem(sys.modules, 'endesive.xades', fake_xades)
+
+    xml_path = tmp_path / 'factura.xml'
+    xml_path.write_text('<factura></factura>', encoding='utf-8')
+    xml_firmado = tmp_path / 'factura_firmada.xml'
+
+    firmador_xades.firmar_xml_xades_bes(str(xml_path), str(xml_firmado), empresa=empresa)
+
+    assert dummy_private_key.algoritmos, 'La llave privada debe usarse al firmar'
+    for algorithm in dummy_private_key.algoritmos:
+        assert isinstance(algorithm, hashes.SHA1)
+
+    tree = etree.parse(str(xml_firmado))
+    ns = {'ds': 'http://www.w3.org/2000/09/xmldsig#'}
+    digest_value = tree.find('.//ds:DigestValue', namespaces=ns)
+    assert digest_value is not None
+
+    target_copy = deepcopy(tree.getroot())
+    signature_inside = target_copy.find('.//ds:Signature', namespaces=ns)
+    if signature_inside is not None and signature_inside.getparent() is not None:
+        signature_inside.getparent().remove(signature_inside)
+
+    digest_buffer = io.BytesIO()
+    etree.ElementTree(target_copy).write_c14n(
+        digest_buffer,
+        exclusive=True,
+        with_comments=False,
+    )
+    digest_expected = base64.b64encode(hashlib.sha1(digest_buffer.getvalue()).digest()).decode()
+    assert digest_value.text == digest_expected
+
+    opciones.firma_electronica.delete(save=False)
+    opciones.delete()
+    empresa.delete()
